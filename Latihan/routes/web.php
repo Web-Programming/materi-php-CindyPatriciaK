2e9bdf0a261fef53303b93f0a6931be036bd3c0d
jojoooook <cindypatriciakadir_2327240008@mhs.mdp.ac.id>
2025-04-29 20:32:35 +0700

    latihan php lagi

--- Latihan/routes/web.php
+++ Latihan/routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('beranda', ['Name' => 'CAKEBYCPK', 'email' => 'cpk@gmail.com', 'Alamat' => 'Sako']);
+});
+
+Route::get('/berita/{id}/{judul?}', function ($id, $judul = null) {
+    return view('berita', ['id' => $id, 'judul' => $judul]);
+
+});
